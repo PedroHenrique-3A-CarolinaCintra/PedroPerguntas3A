54cab6dd55eb00ce59069e1ab642db4c16a7f463
PedroHenrique-3A-CarolinaCintra <00001121612738sp@al.educacao.sp.gov.br>
2024-12-04 14:21:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Minecraft',
+    'Como fazer pão?',
+    '3 Trigos na horizontal'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é foto?',
+    'Luz do sol'
+)
+
+criaCartao(
+    'Cavalo',
+    'Qual o nome do cavalo de Napoleao?',
+    'Marengo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'goodbye'
+)
+
+criaCartao(
+    'Historia',
+    'O que é um senhor feudal?',
+    'Um rei de um feudo'
+)
+
+criaCartao(
+    'Galaxia',
+    'Qual a galaxia mais proxima da nossa?',
+    'Andrômeda'
+)
+
+criaCartao(
+    'Biologia',
+    'Como é o nome cientifico do Gorila?',
+    'Gorilla gorilla'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que é um Horse?',
+    'Cavalo'
+)
+
+criaCartao(
+    'Arte',
+    'O que é o metodo de Loomis?',
+    'Uma maneira de desenhar cabeças'
+)
+
+criaCartao(
+    'física',
+    'Qual o nome da primeira lei de newton?',
+    'Lei da Inércia'
+)
